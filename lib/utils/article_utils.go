6b3cf6aec41958d19d8eb4b4a1a358189d1b266d
Coteh <james_c_34@live.com>
2019-08-22 17:56:25 -0400

    Update wpm

--- lib/utils/article_utils.go
+++ lib/utils/article_utils.go
@@ -1,6 +1,6 @@
 package utils
 
-const wpm = 275
+const wpm = 265
 
 // CalculateExpectedReadTime calculates approx. read time in minutes based on Medium's estimate
 // https://help.medium.com/hc/en-us/articles/214991667-Read-time
